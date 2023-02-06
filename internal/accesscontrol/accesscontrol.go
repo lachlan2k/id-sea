d00eebbfa46121529c6c97c033789acf82b461cb
Lachlan <lachlan@lachlandavidson.com>
2023-02-07 11:09:02 +1300

    Refactor access control to be more efficient

--- internal/accesscontrol/accesscontrol.go
+++ internal/accesscontrol/accesscontrol.go
@@ -23,43 +23,68 @@ func VerifyRedirectURL(conf *config.Config, urlStr string) bool {
 	return false
 }
 
+func HasMandatoryRole(conf *config.Config, email string, roleClaims []string) bool {
+	// If none specified, return true
+	if conf.AccessControl.MandatoryRole == "" {
+		return true
+	}
+
+	if utils.SliceHasMatch(roleClaims, conf.AccessControl.MandatoryRole, true) {
+		return true
+	}
+
+	if roleListForUser, ok := conf.AccessControl.RoleMapping[email]; ok {
+		if utils.SliceHasMatch(roleListForUser, conf.AccessControl.MandatoryRole, true) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func RoleACLMatchesHost(conf *config.Config, allRoles []string, hostname string) bool {
+	for _, roleName := range allRoles {
+		if roleACL, ok := conf.AccessControl.ACLs[roleName]; ok {
+			// TODO: invert this matcher so that the role ACL is fuzzy, not the hostname
+			if utils.SliceHasMatch(roleACL, hostname, true) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
 func CheckAccess(conf *config.Config, email string, roles []string, hostname string) error {
-	// Check access control
 	if !conf.AccessControl.AllowAllEmails {
 		if !utils.SliceHasMatch(conf.AccessControl.EmailAllowlist, email, true) {
 			return fmt.Errorf("user was successfully auth'd (%s), but their email wasn't in the allow list", email)
 		}
 	}
 
-	// Role merging is applied here, instead of at auth time, because if the role mapping is changed and the server is restarted
-	// Then we don't want a user with previous roles to still auth with their JWT
-	allOfUsersRoles := make([]string, len(roles))
-	copy(allOfUsersRoles, roles)
-
-	if roleListForUser, ok := conf.AccessControl.RoleMapping[email]; ok {
-		allOfUsersRoles = append(allOfUsersRoles, roleListForUser...)
-	}
-
-	if conf.AccessControl.MandatoryRole != "" && !utils.SliceHasMatch(allOfUsersRoles, conf.AccessControl.MandatoryRole, true) {
+	// This will be checked when they first login.
+	// However, if the config changes, and a user still has an active session, checking here again will ensure they don't slip through.
+	if !HasMandatoryRole(conf, email, roles) {
 		return fmt.Errorf("user (%s) does not have mandatory role %s", email, conf.AccessControl.MandatoryRole)
 	}
 
-	if !conf.AccessControl.DisableACLRules {
-		isAllowed := false
+	if conf.AccessControl.DisableACLRules {
+		// We've done all checks necessary
+		return nil
+	}
 
-		for _, roleName := range allOfUsersRoles {
-			if roleACL, ok := conf.AccessControl.ACLs[roleName]; ok {
-				if utils.SliceHasMatch(roleACL, hostname, true) {
-					isAllowed = true
-					break
-				}
-			}
-		}
+	if RoleACLMatchesHost(conf, roles, hostname) {
+		// ACLs for our claimed roles matches
+		return nil
+	}
 
-		if !isAllowed {
-			return fmt.Errorf("user (%s) tried to access a hostname they are not authorised to accces (%s)", email, hostname)
+	if mappedRoles, ok := conf.AccessControl.RoleMapping[email]; ok {
+		if RoleACLMatchesHost(conf, mappedRoles, hostname) {
+			// ACLs matched one of our mapped roles
+			return nil
 		}
 	}
 
-	return nil
+	// None of our ACLs matched
+	return fmt.Errorf("user (%s) tried to access a hostname they are not authorised to accces (%s)", email, hostname)
 }
