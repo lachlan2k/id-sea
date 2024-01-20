08b72a51a7aa19e8e6ed1d7cb8bf01da3e32c5d1
Lachlan Davidson <lachlan@lachlandavidson.com>
2024-01-21 01:25:13 +1300

    Update oidc.go

--- internal/webserver/oidc.go
+++ internal/webserver/oidc.go
@@ -71,7 +71,7 @@ func extractRolesFromClaim(conf *config.Config, claims map[string]any) ([]string
 		return nil, fmt.Errorf("couldn't cast roles %v (type of %T) to []any", roleClaim, roleClaim)
 	}
 
-	roles := make([]string, 0, len(rolesAny))
+	roles := make([]string, len(rolesAny))
 	for i, v := range rolesAny {
 		roleStr, ok := v.(string)
 		if !ok {
